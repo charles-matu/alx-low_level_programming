fdc142c947ac4f78f2487ee4be79f7444839d8a3
charles-matu <matucharleswachira@gmail.com>
2023-06-16 04:15:45 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Program to print alphabet letters followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		letter = tolower(letter); // Convert the letter to lowercase
+		putchar(letter); // Print the current letter
+	}
+	putchar('\n'); // Print a new line after printing all the letters
+
+	return (0);
+}
+
