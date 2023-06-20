ba7f1797b99973e1f494825357410874890c4538
charles-matu <matucharleswachira@gmail.com>
2023-06-20 10:56:41 -0700

    ME 2

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,39 +1,22 @@
 #include <stdio.h>
 
 /**
- * natural_sum - Computes the sum of multiples of 3 or 5 below a given limit.
- * @limit: The upper limit (exclusive) for finding multiples.
- *
- * Return: The sum of the multiples of 3 or 5 below the given limit.
- */
-int natural_sum(int limit)
-{
-	int sum = 0;
-	int i;
-
-	for (i = 0; i < limit; i++)
-	{
-		if (i % 3 == 0 || i % 5 == 0)
-		{
-			sum += i;
-		}
-	}
-
-	return (sum);
-}
-
-/**
- * main - Entry point of the program.
+ * main - Lists all the natural numbers below 1024 (excluded)
+ *        that are multiples of 3 or 5.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int limit = 1024;
-	int sum = natural_sum(limit);
+	int i, sum = 0;
 
-	printf("Sum of multiples of 3 or 5 below %d: %d\n", limit, sum);
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
 
 	return (0);
 }
-
