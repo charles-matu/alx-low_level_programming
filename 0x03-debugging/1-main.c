64441f6ca953f44d120079344b6d101e7d5d7a86
charles-matu <matucharleswachira@gmail.com>
2023-06-21 01:46:50 -0700

    Debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0; /* Set the value of i to 0 */
+	positive_or_negative(i); /* Call positive_or_negative function */
+
+	return (0);
+}
+
