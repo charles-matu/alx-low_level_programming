bb4a916c9916322ba28a3fc3736ecef4aec9c266
charles-matu <charleswachiramatu@gmail.com>
2023-06-26 06:26:15 -0700

    prints half a string2

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -4,11 +4,6 @@
  * puts_half - Prints half of a string.
  * @str: The string to be processed.
  *
- * Description: This function prints the second half of a given string.
- *              If the length of the string is odd, it prints from the middle
- *              character onwards. If the length is even, it prints from the
- *              character after the middle character onwards.
- *
  * Return: void
  */
 void puts_half(char *str)
@@ -16,15 +11,15 @@ void puts_half(char *str)
 	int length = 0;
 	int start;
 
-	while (str[length] != '\0') /* Find the length of the string */
+	while (str[length] != '\0')
 		length++;
 
-	if ((length + 1) % 2 != 0) /* Determine the starting index based on length */
+	if ((length + 1) % 2 != 0)
 		start = (length - 1) / 2;
 	else
 		start = length / 2;
 
-	for (; str[start] != '\0'; start++) /* Print the second half of the string */
+	for (; str[start] != '\0'; start++)
 		_putchar(str[start]);
 
 	_putchar('\n');
