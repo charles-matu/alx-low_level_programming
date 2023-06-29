21f7ba44f57d22fd3709c8be9bfd29f433cf9fd6
charles-matu <charleswachiramatu@gmail.com>
2023-06-29 09:52:49 -0700

    hello again 7

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Entry point of the program
  *
- * Description: Modifies a given variable to a specific value
- * without directly referencing the variable or modifying a pointer.
+ * Description:
+ * This program demonstrates a magic trick by modifying the value
+ * at a specific memory location without directly using the variable.
+ * The goal is to assign the value 98 to the element at index 5 of
+ * the integer array 'a' by utilizing a pointer 'p'.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (indicating success)
  */
 int main(void)
 {
@@ -20,15 +23,16 @@ int main(void)
 	/*
 	 * Write your line of code here...
 	 * Remember:
-	 * - You are not allowed to use the variable "a".
-	 * - You are not allowed to modify the variable "p".
-	 * - Only one statement.
-	 * - You are not allowed to code anything else than this line of code.
+	 * - You are not allowed to use 'a'
+	 * - You are not allowed to modify 'p'
+	 * - Only one statement is allowed
+	 * - You are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
 
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
+
 	return (0);
 }
 
