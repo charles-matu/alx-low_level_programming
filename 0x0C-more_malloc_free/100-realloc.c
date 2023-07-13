0b5be2c583598fb53c5d3c5c0abf755c96ef9d6a
charles-matu <matucharleswachira@gmail.com>
2023-07-13 13:02:57 -0700

    advanced tasks 101

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,41 +1,40 @@
 #include "main.h"
 
 /**
- * _realloc - Reallocates memory block.
+ * _realloc - Reallocate memory block.
  *
- * @ptr: Pointer to block.
- * @old_size: Old size in bytes.
- * @new_size: New size in bytes.
+ * @ptr: Pointer to the memory previously allocated.
+ * @old_size: Size of the allocated space for ptr.
+ * @new_size: Size of the new memory block.
  *
- * Return: Pointer to reallocated block, or NULL.
+ * Return: Pointer to the newly allocated memory, or NULL.
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *new_ptr; /* New block pointer */
-	unsigned int i, copy_size; /* Loop counter and copy size */
-	char *old_ptr = ptr; /* Old block pointer */
+	char *p;
+	unsigned int i, n = new_size;
+	char *oldp = ptr;
 
-	if (new_size == old_size)
-		return (ptr); /* Sizes are the same, return original pointer */
-
-	if (new_size == 0 && ptr != NULL)
+	if (ptr == NULL) /* Check if pointer is NULL */
 	{
-		free(ptr);
-		return (NULL); /* Free memory block and return NULL */
+		p = malloc(new_size); /* Allocate new memory block */
+		return (p);
 	}
-
-	new_ptr = malloc(new_size); /* Allocate memory for new block */
-	if (new_ptr == NULL)
-		return (NULL); /* Memory allocation failed, return NULL */
-
-	copy_size = old_size; /* Determine copy size */
-	if (new_size < old_size)
-		copy_size = new_size;
-
-	for (i = 0; i < copy_size; i++)
-		new_ptr[i] = old_ptr[i]; /* Copy contents to new block */
-
-	free(ptr); /* Free old block */
-	return (new_ptr); /* Return pointer to new block */
+	else if (new_size == 0) /* Check if new size is 0 */
+	{
+		free(ptr); /* Free the previous memory block */
+		return (NULL);
+	}
+	else if (new_size == old_size) /* Check if sizes are equal */
+		return (ptr);
+	p = malloc(new_size); /* Allocate new memory block */
+	if (p == NULL)
+		return (NULL);
+	if (new_size > old_size)
+		n = old_size;
+	for (i = 0; i < n; i++) /* Copy data from old block to new block */
+		p[i] = oldp[i];
+	free(ptr); /* Free the previous memory block */
+	return (p);
 }
 
