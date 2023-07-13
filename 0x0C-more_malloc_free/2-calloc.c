c484ef2db84267222e67673cd230698db20d7beb
charles-matu <matucharleswachira@gmail.com>
2023-07-13 09:57:01 -0700

    new coding style 4

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,30 +11,31 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *old_ptr = ptr;
-	char *new_ptr;
-	unsigned int i;
+	char *old_ptr = ptr; /* Original pointer to the memory block */
+	char *new_ptr; /* New pointer for the reallocated memory */
+	unsigned int i; /* Loop counter */
 
 	if (ptr == NULL)
-		return (malloc(new_size));
+		return (malloc(new_size)); /* Allocate new memory block */
 
 	if (new_size == old_size)
-		return (ptr);
+		return (ptr); /* No need to reallocate, return original pointer */
 
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-		return (NULL);
+		return (NULL); /* Free memory block and return NULL */
 	}
 
-	new_ptr = malloc(new_size);
+	new_ptr = malloc(new_size); /* Allocate new memory block */
 	if (new_ptr == NULL)
-		return (NULL);
+		return (NULL); /* Allocation failed, return NULL */
 
+	/* Copy contents from the old memory block to the new one */
 	for (i = 0; i < old_size && i < new_size; i++)
 		new_ptr[i] = old_ptr[i];
 
-	free(ptr);
-	return (new_ptr);
+	free(ptr); /* Free the old memory block */
+	return (new_ptr); /* Return the new allocated memory block */
 }
 
