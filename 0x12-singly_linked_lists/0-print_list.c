db699c35bdf988a592564ee9c834f140c4ca5807
charles-matu <matucharleswachira@gmail.com>
2023-07-27 04:21:16 -0700

    list correction

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,28 +6,26 @@
 /**
  * print_list - Print elements of a linked list.
  * @h: Pointer to the head of the list.
+ * Return: Number of nodes.
  *
- * Description:
- * This function prints the content and length of each node in the linked list.
- * If the string in a node is NULL, it prints "[0] (nil)". The function returns
- * the total number of nodes in the list.
- *
- * Return: The number of nodes in the list.
+ * Description: Traverse the linked list and print each node's content and length.
+ * Print "[0] (nil)" for NULL strings.
  */
 size_t print_list(const list_t *h)
 {
-	int count = 0; /* Count nodes. */
+    int count = 0; // Initialize node count.
 
-	while (h)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n"); /* Format for NULL strings. */
-		else
-			printf("[%d] %s\n", h->len, h->str); /* Format for non-NULL strings. */
+    while (h)
+    {
+        if (h->str == NULL)
+            printf("[0] (nil)\n"); // Format for NULL strings.
+        else
+            printf("[%d] %s\n", h->len, h->str); // Format for non-NULL strings.
 
-		count++; /* Increment node count. */
-		h = h->next; /* Move to the next node. */
-	}
-	return (count); /* Return node count. */
+        count++; // Increment node count.
+        h = h->next; // Move to the next node.
+    }
+
+    return (count); // Return node count.
 }
 
